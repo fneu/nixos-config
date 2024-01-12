da9e94088b4da029b99bc89d20ba5f1f4fecb2dc
Fabian Neuschmidt <fabian@neuschmidt.de>
2024-01-12 18:42:15 +0100

    fix: add redundant allow unfree option

--- modules/base.nix
+++ modules/base.nix
@@ -31,9 +31,6 @@
     packages = with pkgs; [];
   };
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
-
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
