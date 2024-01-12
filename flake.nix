eddbc00ba6b36f6d28cafa6dc5f55e7c357eab36
= <=>
2024-01-12 19:52:51 +0100

    feat: add home-manager

--- flake.nix
+++ flake.nix
@@ -7,9 +7,13 @@
       url = "github:nix-community/NixOS-WSL";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, nixos-wsl }: 
+  outputs = { self, nixpkgs, nixos-wsl, home-manager }@inputs: 
   let
     system = "x86_64-linux";
 
@@ -22,17 +26,31 @@
     };
   in
   {
+
     nixosConfigurations = {
       P51 = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit system; };
 	modules = [
-          ./hosts/P51/default.nix
+          ./hosts/P51
+
+  	  home-manager.nixosModules.home-manager
+  	  {
+  	    home-manager.extraSpecialArgs = inputs;
+  	    home-manager.users.fabian = import ./home.nix;
+  	  }
 	];
       };
       WSL = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit system; };
 	modules = [
-	  ./hosts/WSL/default.nix
+	  ./hosts/WSL
+
+  	  home-manager.nixosModules.home-manager
+  	  {
+  	    home-manager.extraSpecialArgs = inputs;
+  	    home-manager.users.fabian = import ./home.nix;
+  	  }
+
 	  nixos-wsl.nixosModules.wsl
 	];
       };
