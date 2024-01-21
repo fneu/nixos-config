4c1d1edc212ff53f7a6ba8f0e48c7ee24f35de8b
Fabian Neuschmidt <fabian.neuschmidt@icarus-consult.de>
2024-01-21 15:12:19 +0100

    feat: bluetooth (audio)

--- modules/hyprland.nix
+++ modules/hyprland.nix
@@ -39,4 +39,18 @@
     pulse.enable = true;
     jack.enable = true;
   };
+
+  #Bluetooth
+  hardware.bluetooth.enable = true;
+  hardware.bluetooth.powerOnBoot = true;
+  services.blueman.enable = true;
+
+  # Media Buttons
+  systemd.user.services.mpris-proxy = {
+    description = "Mpris proxy";
+    after = ["network.target" "sound.target"];
+    wantedBy = ["default.target"];
+    serviceConfig.ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
+  };
 }
+
